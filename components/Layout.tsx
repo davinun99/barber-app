5a543c6304733632354513d8b72ce50b8068dd06
David <nundavid99@gmail.com>
2023-11-11 02:00:47 -0300

    Add new page and update layout and footer components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,24 +5,19 @@ import { Country } from "@typings/models";
 
 type Props = {
   children: React.ReactNode;
-  lang: string;
-  countryCode: string;
-  buildLanguages?: Country[];
-  countries?: Country[];
-  pageTitle?: string;
 };
 
-const Layout: React.FC<Props> = ({ children, buildLanguages = [], lang, countryCode, countries = [] }) => {
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <div className="relative bg-ashy overflow-hidden">
         <div className="relative pt-5 pb-10 px-5 lg:px-0 lg:pb-16 max-w-screen-lg mx-auto">
-          <Header lang={lang} countryCode={countryCode} />
+          <Header />
         </div>
 
         <main>{children}</main>
 
-        <Footer lang={lang} countries={countries} buildLanguages={buildLanguages} />
+        <Footer />
       </div>
     </>
   );
---
+++ pages/mopri/index.tsx
@@ -0,0 +1,12 @@
+import Page from "@components/Page";
+import React from "react"
+
+const index = () => {
+  return (
+    <Page>
+      xd
+    </Page>
+  );
+};
+
+export default index;
