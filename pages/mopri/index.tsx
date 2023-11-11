ec120bf741cb65c69a14ac2ad5dec852bf13e5fa
David <nundavid99@gmail.com>
2023-11-11 02:59:59 -0300

    initial lista reservas

--- pages/mopri/index.tsx
+++ pages/mopri/index.tsx
@@ -1,12 +1,32 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import ListaReservas from "@components/ListaReservas";
 import Page from "@components/Page";
-import React from "react"
+import { Reserva } from "@typings/models";
+import sanityApi from "@utils/sanity/api";
 
-const index = () => {
+type Props = {
+  reservas: Reserva[];
+};
+
+const index: NextPage<Props> = ({ reservas }) => {
   return (
     <Page>
-      xd
+      <div className="px-3 lg:px-0 container mx-auto max-w-screen-lg flex flex-wrap sm:flex-nowrap">
+        <ListaReservas reservas={reservas} />
+      </div>
     </Page>
   );
 };
 
 export default index;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const reservas = await sanityApi.getAllReservas();
+  return {
+    props: {
+      reservas
+    },
+    revalidate: 60
+  };
+}
