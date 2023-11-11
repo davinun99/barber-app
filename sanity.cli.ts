48bdf12b2689f4834bdbe57847ee7d9ec68e6078
David <nundavid99@gmail.com>
2023-11-11 01:21:22 -0300

    vite vars and dotenv invocation in cli

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-process-env */
 import { loadEnvConfig } from "@next/env";
 import { defineCliConfig } from "sanity/cli";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const dev = process.env.NODE_ENV !== "production";
 loadEnvConfig(__dirname, dev, { info: () => null, error: console.error });
