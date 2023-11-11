45dde9e95f055a71fd1e4661da4ba1abe706e753
David <nundavid99@gmail.com>
2023-11-11 02:58:24 -0300

    Add Flowbite library and update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./node_modules/flowbite-react/**/*.js", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: { ashy: "#fdfbfb" }
     }
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")]
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio"), require("flowbite/plugin")]
 };
