73852d7c678b2eed09c5d84fe64136edc132ba20
David <nundavid99@gmail.com>
2023-11-11 02:11:35 -0300

    Update environment variables in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,10 +4,10 @@ import { visionTool } from "@sanity/vision";
 import schemas from "./schemas/schemaTypes";
 import { Logo } from "./plugins/studioLogo";
 
-const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE;
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const title = process.env.SANITY_STUDIO_PROJECT_TITLE || process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE;
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.SANITY_STUDIO_API_VERSION || process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export default defineConfig({
   title,
